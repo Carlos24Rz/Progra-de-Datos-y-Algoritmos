afd7d5c40bb46e627c4ecee88176fa0caf05e796
Carlos24Rz <85084188+Carlos24Rz@users.noreply.github.com>
2021-09-03 14:50:06 -0500

    Update main.cpp

--- Tarea 2/main.cpp
+++ Tarea 2/main.cpp
@@ -3,12 +3,30 @@
 
 using namespace std;
 
-void ordenaIntercambio(vector<int> v)
+void ordenaIntercambio(vector<int> v) //Carlos
 {
 
 }
 
+void ordenaBurbuja(vector<int> v) //Ángel
+{
 
+}
+
+void ordenaMerge(vector<int> v) //Carlos
+{
+
+}
+
+int busqSecuencial(vector<int> v, int x) //Ángel
+{
+
+}
+
+int busqBinaria (vector<int> v, int x) //Carlos
+{
+
+}
 
 
 int main()
