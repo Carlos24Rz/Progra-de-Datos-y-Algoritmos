a240f8d97bdfb4b06f311947029ea4c1d2b77958
Carlos24Rz <85084188+Carlos24Rz@users.noreply.github.com>
2021-09-11 09:17:12 -0500

    Funcion para guardar vector completamente implementado

--- Actividad 1.3/main.cpp
+++ Actividad 1.3/main.cpp
@@ -7,6 +7,7 @@
 
 using namespace std;
 
+//Funcion que guarda contenido de archivo txt en un vector
 vector<Registro> leerArchivo()
 {
     vector<Registro> registro;
@@ -16,11 +17,12 @@ vector<Registro> leerArchivo()
 
     if (s.is_open())
     {
-        
         while (getline(s, str))
         {
+
             stringstream ss(str);
-            string fecha[3];
+            string fechas[3];
+            string fecha;
             string string[5];
             
             for(int i = 0; i < 5; i++)
@@ -31,20 +33,21 @@ vector<Registro> leerArchivo()
                 else
                     getline(ss, string[i], ' ');
             }
+            
+            fecha = string[2];
 
-
-            stringstream sss(string[2]);
+            stringstream sss(fecha);
 
             for(int i = 0; i < 3; i++){
-                getline(sss,fecha[3], ';');
+                getline(sss, fechas[i], ':');
             }
 
             Registro r(
                 string[0],
                 stoi(string[1]),
-                stoi(fecha[0]),
-                stoi(fecha[1]),
-                stoi(fecha[2]),
+                stoi(fechas[0]),
+                stoi(fechas[1]),
+                stoi(fechas[2]),
                 string[3],
                 string[4]
             );
@@ -59,9 +62,8 @@ vector<Registro> leerArchivo()
 
 int main()
 {
-
     vector<Registro> r = leerArchivo();
-    cout << r[0].getRegistro() << endl;
+    
     return 1; 
 }
 
--- Actividad 1.3/registro.h
+++ Actividad 1.3/registro.h
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <iomanip>
+#include <sstream>
 
 using namespace std;
 
@@ -12,14 +14,14 @@ class Registro{
     public:
         Registro(string mes, int dia, int hora, int minuto, int segundo, string ip, string log)
         {
-            if(mes == "Ene") m_mes = 1;
+            if(mes == "Jan") m_mes = 1;
             else if(mes == "Feb") m_mes = 2;
             else if(mes == "Mar") m_mes = 3;
-            else if(mes == "Abr") m_mes = 4;
+            else if(mes == "Apr") m_mes = 4;
             else if(mes == "May") m_mes = 5;
             else if(mes == "Jun") m_mes = 6;
             else if(mes == "Jul") m_mes = 7;
-            else if(mes == "Ago") m_mes = 8;
+            else if(mes == "Aug") m_mes = 8;
             else if(mes == "Sep") m_mes = 9;
             else if(mes == "Oct") m_mes = 10;
             else if(mes == "Nov") m_mes = 11;
@@ -75,7 +77,7 @@ class Registro{
             switch (m_mes)
             {
             case 1:
-                mes = "Ene";
+                mes = "Jan";
                 break;
             case 2:
                 mes = "Feb";
@@ -84,7 +86,7 @@ class Registro{
                 mes = "Mar";
                 break;
             case 4:
-                mes = "Abr";
+                mes = "Apr";
                 break;
             case 5:
                 mes = "May";
@@ -96,7 +98,7 @@ class Registro{
                 mes = "Jul";
                 break;
             case 8:
-                mes = "Ago";
+                mes = "Aug";
                 break;
             case 9:
                 mes = "Sep";
@@ -108,12 +110,16 @@ class Registro{
                 mes = "Nov";
                 break;
             case 12:
-                mes = "Dic";
+                mes = "Dec";
                 break;
             }
             
+            stringstream ss;
+
+            ss << setfill('0') << setw(2) << to_string(m_dia);
+
             string str = mes + " " 
-            + to_string(m_dia) + " " 
+            + ss.str() + " " 
             + to_string(m_hora) + ":" 
             + to_string(m_minuto) + ":" 
             + to_string(m_segundo) + " "
