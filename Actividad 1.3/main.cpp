f4d84e497ace67851a81d8684693a797214a93ef
Carlos24Rz <85084188+Carlos24Rz@users.noreply.github.com>
2021-09-15 00:15:21 -0500

    Implementado mergeSort

--- Actividad 1.3/main.cpp
+++ Actividad 1.3/main.cpp
@@ -60,10 +60,68 @@ vector<Registro> leerArchivo()
     return registro;
 }
 
+void merge(vector<Registro> &v, int l, int m, int r) {
+   int i, j, k, nl, nr;
+   //size of left and right sub-arrays
+   nl = m-l+1; nr = r-m;
+   vector<Registro> larr,rarr;
+   //fill left and right sub-arrays
+   for(i = 0; i<nl; i++)
+      larr.push_back(v[l+i]);
+   for(j = 0; j<nr; j++)
+      rarr.push_back(v[m+1+j]);
+   i = 0; j = 0; k = l;
+   //marge temp arrays to real array
+   while(i < nl && j<nr) {
+      if(larr[i] <= rarr[j]) {
+         v[k] = larr[i];
+         i++;
+      }else{
+         v[k] = rarr[j];
+         j++;
+      }
+      k++;
+   }
+   while(i<nl) {       //extra element in left array
+      v[k] = larr[i];
+      i++; k++;
+   }
+   while(j<nr) {     //extra element in right array
+      v[k] = rarr[j];
+      j++; k++;
+   }
+}
+
+void mergeSort(vector<Registro> &v, int l, int r) {
+   int m;
+   if(l < r) {
+      int m = l+(r-l)/2;
+      // Sort first and second arrays
+      mergeSort(v, l, m);
+      mergeSort(v, m+1, r);
+      merge(v, l, m, r);
+   }
+}
+
 int main()
 {
     vector<Registro> r = leerArchivo();
+    
+    int n = r.size();
+    
+    mergeSort(r, 0, n-1);
 
-    return 1; 
+    ofstream Myfile("ordenado.txt");
+
+    for (int i = 0; i < n; i++)
+    {
+      Myfile << r.at(i).getRegistro() << endl;
+    }
+
+    Myfile.close();
+    
+    
+
+    return 0; 
 }
 
