2b15bb3b5928e1a288545f53497ea1ae280678bb
Carlos24Rz <85084188+Carlos24Rz@users.noreply.github.com>
2021-10-22 16:12:05 -0500

    Metodo Insertar implementado correctamente

---
+++ Actividad-3/Act-3.1/Act-3.4/main.cpp
@@ -0,0 +1,131 @@
+// Programa que trabaja con una lista doblemente ligada de nodos
+// Carlos Daniel Diaz Arrazate - A01734902
+// Jose Angel Gonzalez Carrera - A01552274
+// Carlos Eduardo Ruiz Lira - A01735706
+// 16/10/21
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include "registro.h"
+#include <vector>
+using namespace std;
+
+struct Node
+{
+    Registro data;
+    struct Node *left;
+    struct Node *right;
+};
+
+struct Node *newNode(Registro data)
+{
+    struct Node *temp;
+    temp = (Node *)malloc(sizeof(Node));
+
+    temp->data = data;
+    temp->left = NULL;
+    temp->right = NULL;
+    return temp;
+}
+
+// Descripcion: Insertar de nodo
+// Entrada: Referencia del apuntador a la raiz del arbol, dato a insertar
+// Salida: Nada
+// Complejidad: O(n)
+void Insertar(struct Node *&root, Registro data)
+{
+    if (root == NULL)
+    {
+        root = newNode(data);
+        return;
+    }
+    else
+    {
+        if (data < root->data)
+        {
+            if (root->left == NULL)
+                root->left = newNode(data);
+            else
+                Insertar(root->left, data);
+        }
+        else if (data > root->data)
+        {
+            if (root->right == NULL)
+                root->right = newNode(data);
+            else
+                Insertar(root->right, data);
+        }
+    }
+}
+
+// Descripcion: Lectura del archivo para la creacion de la lista ligada
+// Entrada: Apuntador a root de lista ligada
+// Salida: Nada
+// Complejidad: O(n)
+void leerArchivo(struct Node *&root)
+{
+    string line;
+    ifstream file("bitacora.txt");
+    if (file.is_open())
+    {
+        while (getline(file, line))
+        {
+            stringstream ss(line); // Renglon individual
+            string fechas[3];
+            string fecha;
+            string string[5];
+            for (int i = 0; i < 5; i++)
+            {
+                if (i == 4)
+                    getline(ss, string[i]);
+                else
+                    getline(ss, string[i], ' ');
+            }
+            fecha = string[2];
+            stringstream sss(fecha);
+            for (int i = 0; i < 3; i++)
+            {
+                getline(sss, fechas[i], ':');
+            }
+
+            Registro r(
+                string[0],
+                stoi(string[1]),
+                stoi(fechas[0]),
+                stoi(fechas[1]),
+                stoi(fechas[2]),
+                string[3],
+                string[4]);
+
+            Insertar(root, r);
+        }
+    }
+}
+
+// Descripcion: Imprimir en inorden
+// Entrada: Apuntador a la raiz del arbol
+// Salida: Nada
+// Complejidad: O(n)
+void InorderConvexo(struct Node *root, vector<Registro> &registro)
+{
+    if (root == NULL)
+        return;
+    InorderConvexo(root->right, registro);
+    registro.push_back(root->data);
+    InorderConvexo(root->left, registro);
+}
+
+int main()
+{
+    struct Node *root = NULL;
+    leerArchivo(root);
+
+    cout << "--------------------" << endl;
+    vector<Registro> registros;
+    InorderConvexo(root, registros);
+
+    for (int i = 0; i < 5; i++)
+    {
+        cout << registros[i].getRegistro() << endl;
+    }
+}
