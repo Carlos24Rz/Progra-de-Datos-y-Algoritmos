44b7d4a0e8cf10757f9263c85825e769ff6ad36b
J Angel Glez <a01552274@itesm.mx>
2021-10-29 02:16:14 -0500

    subiendo carpeta 3.2

---
+++ Actividad-3/Act-3.2/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+class priority_queue{
+
+    private:
+        int front;
+        int rear;
+        // int[MAX];
+        
+    public:
+        void push(int data);
+        void pop();
+        int top();
+        bool empty();
+        int size();
+    
+};
+
+
+
+int main(){
+
+    
+
+
+    return 0;
+}
+
