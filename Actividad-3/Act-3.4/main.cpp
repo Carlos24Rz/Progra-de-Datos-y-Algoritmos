e03161a8980b5637fb4ebc810c2c1af116949369
JAngelGC <a01552274@itesm.mx>
2021-11-06 17:37:01 -0600

    Cambiando metodo Insertar por uno iterativo

--- Actividad-3/Act-3.4/main.cpp
+++ Actividad-3/Act-3.4/main.cpp
@@ -57,33 +57,62 @@ void Insertar(struct Node *&root, Registro data)
   }
 }
 
+
+Node* insert(Node* root, Registro data)
+{
+    // Crear el nuevo nodo a agregar
+    Node* newnode = newNode(data);
+ 
+    // Pointer que recorre el arbol
+    Node* temp = root;
+ 
+    // Pointer father mantiene el nodo al que se le debe insertar un hijo
+    Node* father = NULL;
+ 
+    while (temp != NULL) {
+        father = temp;
+        if (data < temp->data)
+            temp = temp->left;
+        else
+            temp = temp->right;
+    }
+ 
+    // Si la raiz es NULL, el arbol esta vacio
+    // El newnode es la raiz
+    if (father == NULL)
+        father = newnode;
+ 
+    // Es un hijo menor
+    else if (data < father->data)
+        father->left = newnode;
+ 
+    // Es un hijo mayor
+    else
+        father->right = newnode;
+ 
+    // Retorna el pointer en donde se inserto el newnode
+    return father;
+}
+
+
 // Descripcion: Lectura del archivo para la creacion del arbol
 // Entrada: Apuntador a root de lista ligada
 // Salida: Nada
 // Complejidad: O(n)
 int n = 0;
-stringstream ss;
-stringstream sss;
-string line;
-string fechas[3];
-string fecha;
-string myString[5];
 
 void leerArchivo(struct Node *&root)
 {
-  // string line;
-  ifstream file("bitacora2MB.txt");
+  string line;
+  ifstream file("bitacora7MB.txt");
   if (file.is_open())
   {
     while (getline(file, line))
     {
-      // stringstream ss(line); // Renglon individual
-      ss << line;
-      // cout << line << endl;
-
-      // string fechas[3];
-      // string fecha;
-      // string myString[5];
+      stringstream ss(line); // Renglon individual
+      string fechas[3];
+      string fecha;
+      string myString[5];
 
       for (int i = 0; i < 5; i++)
       {
@@ -94,18 +123,15 @@ void leerArchivo(struct Node *&root)
       }
 
       fecha = myString[2];
-      // stringstream sss(fecha);
+      stringstream sss(fecha);
       sss << fecha;
-
 
       for (int i = 0; i < 3; i++)
       {
         getline(sss, fechas[i], ':');
       }
 
-      //  Registro(string mes, int dia, int hora, int minuto, int segundo, string ip, string log)
-
-      Registro* myRegister = new Registro(
+      Registro r(
           myString[0],
           stoi(myString[1]),
           stoi(fechas[0]),
@@ -113,33 +139,18 @@ void leerArchivo(struct Node *&root)
           stoi(fechas[2]),
           myString[3],
           myString[4]);
-      // Registro r(
-      //     myString[0],
-      //     stoi(myString[1]),
-      //     stoi(fechas[0]),
-      //     stoi(fechas[1]),
-      //     stoi(fechas[2]),
-      //     myString[3],
-      //     myString[4]);
 
-      // cout << myRegister->getRegistro() << endl;
+    //   Insertar(root, r);      
+    if(n==0)
+        root = insert(root, r);
+    else 
+        insert(root, r);
+
       cout << "Insertando nodo: " << n << endl;
       n++;
-      Insertar(root, *myRegister);
-
-      delete  myRegister;
-      ss.str("");
-      ss.clear(); // Clear state flags.
-      sss.str("");
-      sss.clear(); // Clear state flags.
-      
     }
   }
 }
-
-
-// LLega al 41,526
-
 
 // Descripcion: Almacenar nodos en inorden convexo en un vector
 // Entrada: Apuntador a la raiz del arbol
@@ -155,25 +166,11 @@ void InorderConvexo(struct Node *root, vector<Registro> &registro)
   InorderConvexo(root->left, registro);
 }
 
-// Lineas que matan el codigo de la bitacora: estas lineas son al ir eliminando las de arriba
-// 9666: Apr 02 04:58:48 119.7.10.137:2045 Illegal user
-// 19081: Oct 07 21:51:45 260.558.961.482:5850 Illegal user
-// 19081: Feb 13 13:53:45 188.644.613.104:2026 Illegal user
-// 61074: Oct 07 21:51:09 128.560.636.362:9760 Illegal user
-// 61074: Jun 06 06:16:11 937.937.964.25:8073 Failed password for root
-// 82078: Jan 15 01:05:26 139.176.217.262:1512 Failed password for root
-// 82078: Mar 06 14:52:04 888.709.256.537:5105 Failed password for illegal user test
-
 
 int main()
 {
   struct Node *root = NULL;
   leerArchivo(root);
-
-  //   cout << root->data.getIP() << endl;
-  //   cout << root->left->data.getIP() << endl;
-  //   cout << root->right->data.getIP() << endl;
-  //   cout << root->right->left->left << endl;
 
   cout << "--------------------" << endl;
   vector<Registro> registros;
