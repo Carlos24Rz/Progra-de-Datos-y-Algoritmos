1abb579204ac2fac16bc8fe41404857e063e12d3
JAngelGC <a01552274@itesm.mx>
2021-11-05 23:33:07 -0600

    Archivo para crear bitacoras

---
+++ Actividad-3/Act-3.4/crearBitacora.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <string>
+
+#include <fstream>
+using namespace std;
+
+int numAleatorio(int inferior, int superior, int rnd){
+  srand(rnd);
+  int diferencia = superior - inferior;
+  return (rand() % diferencia) + inferior;
+}
+
+string addZero(string number){
+  return "0" + number;
+}
+
+
+int main(){
+
+    string months[] = {"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"};
+
+    string messages[] {"Failed password for illegal user test",
+                        "Failed password for root",
+                        "Illegal user"};
+
+    // Aug 28 23:07:49 897.53.984.6:6710 Failed password for root
+
+    // int lines = 500000; // 25MB
+    int lines = 250000; // 15MB
+    // int lines = 120000; // 6MB
+    // int lines = 60000; // 3MB
+    // int lines = 25;
+
+    ofstream myfile;
+    myfile.open ("bitacora3MB.txt");
+
+    for(int i=0; i<lines; i++){
+
+        string month = months[numAleatorio(0,11,i)];
+        string day = to_string(numAleatorio(1,29,i));
+        if(stoi(day)<10)
+          day = addZero(day);
+
+        string time1 = to_string(numAleatorio(1,23,i));
+        if(stoi(time1)<10)
+          time1 = addZero(time1);
+
+        string time2 = to_string(numAleatorio(1,59,i));
+        if(stoi(time2)<10)
+          time2 = addZero(time2);
+
+        string time3 = to_string(numAleatorio(1,58,i));
+        if(stoi(time3)<10)
+          time3 = addZero(time3);
+
+        int ip1 = numAleatorio(0,999,i);
+        int ip2 = numAleatorio(0,998,i);
+        int ip3 = numAleatorio(0,997,i);
+        int ip4 = numAleatorio(0,996,i);
+        int port = numAleatorio(0,9998,i);
+        string msg = messages[numAleatorio(0,3,i)];
+        
+        myfile << month << " " << day << " " << time1 << ":" << time2 << ":" << time3 <<
+        " " << ip1 << "." << ip2 << "." << ip3 << "." << ip4 << ":" << port << " " << msg << "\n";
+    }
+
+
+    myfile.close();
+
+    return 0;
+}
--- Actividad-3/Act-3.4/main.cpp
+++ Actividad-3/Act-3.4/main.cpp
@@ -20,7 +20,7 @@ struct Node
 struct Node *newNode(Registro data)
 {
   struct Node *temp;
-  temp = (Node*)malloc(sizeof(Node));
+  temp = (Node *)malloc(sizeof(Node));
 
   temp->data = data;
   temp->left = NULL;
@@ -34,29 +34,38 @@ struct Node *newNode(Registro data)
 // Complejidad: O(n)
 void Insertar(struct Node *&root, Registro data)
 {
-  if (root == NULL) {
+  if (root == NULL)
+  {
     root = newNode(data);
     return;
   }
-  else {
+
+  else
+  {
     if (data < root->data)
-      if(root->left == NULL) root->left = newNode(data);
-      else Insertar(root->left, data);
-    else if (root->data < data )
-      if(root->right == NULL) root->right = newNode(data);
-      else Insertar(root->right, data);
+
+      if (root->left == NULL)
+        root->left = newNode(data);
+      else
+        Insertar(root->left, data);
+
+    else if (root->data < data)
+      if (root->right == NULL)
+        root->right = newNode(data);
+      else
+        Insertar(root->right, data);
   }
 }
 
-
-// Descripcion: Lectura del archivo para la creacion de la lista ligada
+// Descripcion: Lectura del archivo para la creacion del arbol
 // Entrada: Apuntador a root de lista ligada
 // Salida: Nada
 // Complejidad: O(n)
+int n = 0;
 void leerArchivo(struct Node *&root)
 {
   string line;
-  ifstream file("bitacora.txt");
+  ifstream file("bitacora15MB.txt");
   if (file.is_open())
   {
     while (getline(file, line))
@@ -80,15 +89,16 @@ void leerArchivo(struct Node *&root)
       }
 
       Registro r(
-                string[0],
-                stoi(string[1]),
-                stoi(fechas[0]),
-                stoi(fechas[1]),
-                stoi(fechas[2]),
-                string[3],
-                string[4]
-            );
+          string[0],
+          stoi(string[1]),
+          stoi(fechas[0]),
+          stoi(fechas[1]),
+          stoi(fechas[2]),
+          string[3],
+          string[4]);
 
+      cout << "Insertando nodo: " << n << endl;
+      n++;
       Insertar(root, r);
     }
   }
@@ -102,31 +112,38 @@ void InorderConvexo(struct Node *root, vector<Registro> &registro)
 {
   if (root == NULL)
     return;
+
   InorderConvexo(root->right, registro);
   registro.push_back(root->data);
   InorderConvexo(root->left, registro);
-
 }
+
+// Lineas que matan el codigo de la bitacora: estas lineas son al ir eliminando las de arriba
+// 9666: Apr 02 04:58:48 119.7.10.137:2045 Illegal user
+// 19081: Oct 07 21:51:45 260.558.961.482:5850 Illegal user
+// 19081: Feb 13 13:53:45 188.644.613.104:2026 Illegal user
+// 61074: Oct 07 21:51:09 128.560.636.362:9760 Illegal user
+// 61074: Jun 06 06:16:11 937.937.964.25:8073 Failed password for root
+// 82078: Jan 15 01:05:26 139.176.217.262:1512 Failed password for root
+// 82078: Mar 06 14:52:04 888.709.256.537:5105 Failed password for illegal user test
+
 
 int main()
 {
   struct Node *root = NULL;
   leerArchivo(root);
 
-//   cout << root->data.getIP() << endl;
-//   cout << root->left->data.getIP() << endl;
-//   cout << root->right->data.getIP() << endl;
-//   cout << root->right->left->left << endl;
+  //   cout << root->data.getIP() << endl;
+  //   cout << root->left->data.getIP() << endl;
+  //   cout << root->right->data.getIP() << endl;
+  //   cout << root->right->left->left << endl;
 
-cout << "--------------------" << endl;
-vector<Registro> registros;
-InorderConvexo(root, registros);
 
-for (int i = 0; i < 5; i++)
-{
+  vector<Registro> registros;
+  InorderConvexo(root, registros);
+
+  for (int i = 0; i < 5; i++)
+  {
     cout << registros[i].getRegistro() << endl;
-}
-
-  
-  
+  }
 }
