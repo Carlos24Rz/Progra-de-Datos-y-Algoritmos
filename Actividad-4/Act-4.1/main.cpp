f1e3f4d4deba628af89ad78098859e8d4bfad5aa
JAngelGC <a01552274@itesm.mx>
2021-11-12 01:00:24 -0600

    Implementando lista de adyacencias correctamente

--- Actividad-4/Act-4.1/main.cpp
+++ Actividad-4/Act-4.1/main.cpp
@@ -2,86 +2,112 @@
 // Carlos Daniel Diaz Arrazate  - A01734902
 // Jose Angel Gonzalez Carrera  - A01552274
 // Carlos Eduardo Ruiz Lira     - A01735706
-// 17/10/21
-
+// 16/10/21
 #include <iostream>
 #include <string>
 #include <sstream>
 using namespace std;
 
-class NodeRed{
-  NodeBlue* data = NULL;
-  NodeRed* next = NULL;
+// IMPORTANTE, DEBEMOS DECLARAR LAS CLASES PARA QUE SE LLAMEN UNAS A OTRAS
+class NodeRed;
+class NodeBlue;
 
-  NodeRed(){}
-}
+
+
+class NodeRed{
+
+  public:
+    NodeBlue* data = NULL;
+    NodeRed* next = NULL;
+    NodeRed(){};
+    NodeRed(NodeBlue* m_data){
+      data = m_data;
+    };
+
+};
+
 
 class NodeBlue{
 
   public:
-    int data;
+    int index;
     bool status = false;
     NodeRed* next = NULL;
 
-    NodeBlue(){}
-
-    NodeBlue(int m_data){
-      data = m_data;
+    NodeBlue(){
+      status = false;
+      next = NULL;
     }
 
-    // int getData(){
-    //   return data;
-    // }
+    int getData(){
+      return index;
+    }
 
-    // bool getStatus(){
-    //   return status;
-    // }
+    bool getStatus(){
+      return status;
+    }
 
     void process(){
       status = true;
     }
 
-    void setData(int m_data){
-      data = m_data;
+    void setIndex(int m_index){
+      index = m_index;
     }
 
 };
 
 
-void Inserta_al_final(Node* &arrAzul, int blueIndex, int value) {
-  NodeRed* newNode = new NodeRed();
-  Node* last = arrAzul[blueIndex];
+void Inserta_al_final(NodeBlue* arrAzul[], int indexArr, int indexValue){
 
-  newNode.data = arrAzul[value];
+  // Posicion del array al que se le debe insertar un nodo rojo
+  NodeBlue* temp = arrAzul[indexArr];
 
-  if (arrAzul[blueIndex].next == NULL) {
-    arrAzul[blueIndex].next = newNode;
+  // Posicion que debe ser insertada en el nodo rojo
+  NodeBlue* tempToInsert = arrAzul[indexValue];
+  NodeRed* newNodeRed = new NodeRed(tempToInsert);
+
+  // Si mi NodeBlue en la posicion en su posicion no apunta a nada:
+  if(temp->next == NULL){
+    temp->next = newNodeRed;
     return;
   }
 
-  while (last->next != NULL)
-    last = last->next;
+  // En caso de que el NodeBlue en su posicion ya apunte a un NodeRed
+  // Obtenemos el primer NodeRed al que apunta NodeBlue
+  NodeRed* lastRed = temp->next;
 
-  last->next = newNode;
+  // Se recorre lastRed hasta el final de la lista ligada para insertar el nodo
+  while(lastRed->next != NULL)
+    lastRed = lastRed->next;
+
+  lastRed->next = newNodeRed;
+
   return;
 }
 
 
+void ImprimeListaNodeBlue(NodeBlue* tmpBlue, int indexArrAzul){
 
+  cout << char(indexArrAzul + 65);
 
+  // Verificar que el NodeBlue apunte a algo que no sea NULL
+  if(tmpBlue->next != NULL){
+    NodeRed* tmpRed = tmpBlue->next;
 
-void Imprime(Node *tmp) {
-   //traverse the list to display each node
-   while (tmp != NULL)
-   {
-      cout << " - " << char((tmp->data)+65);
-      tmp = tmp->next;
-   }
-   cout << endl;
+    // Imprimir mientras haya NodeRed
+    while(tmpRed != NULL){
+      cout << " - " << char((tmpRed->data->index)+65); // char((tmpRed->data)+65)
+      tmpRed = tmpRed->next;
+    }
+    cout << endl;
+  }
 }
 
-
-
+NodeBlue* createNodeBlue(){
+  NodeBlue* newNodeBlue = new NodeBlue();
+  return newNodeBlue;
+}
 
 // poner ascii con alt
 
@@ -90,35 +116,45 @@ int main() {
   string str, input;
 
   // Crear array
-  cout << "Size: ";
+  // cout << "Size: ";
   cin >> n;
   int matrix[n][n];
 
 
   // Creando array de pointers
-  NodeBlue arrAzul[n];
-
+  NodeBlue* arrAzul[n];
   for(int i=0; i<n; i++){
-    arrAzul[i].data = i;
+    arrAzul[i] = createNodeBlue();
+    arrAzul[i]->setIndex(i); // cada NodeBlue guarda su indice correspondiente al arrAzul
+    cout << i << " - ";
   }
-  
+
   for(int i=0; i<n; i++){
     for(int j=0; j<n; j++){
       cin >> matrix[i][j];
       // Aqui tenemos A en i==0
       if(matrix[i][j] == 1){
+        // Aqui va la magia 
         Inserta_al_final(arrAzul, i, j);
-  
       }
     }
   }
 
   cout << "\n------------" << endl;
-  
+
+
   for(int i=0; i<n; i++){
-    cout << char(i+65);
-    Imprime(arrAzul[i]);
+    ImprimeListaNodeBlue(arrAzul[i], i);
   }
+
+  
+  
+  // for(int i=0; i<n; i++){
+  //   cout << char(i+65);
+  //   Imprime(arrAzul[i]);
+  // }
+
+
   
   
 
